2d280c4208566ea8e84454ab3bf5e1d40c67aac4
AnandBhalla <bhallaanand4@gmail.com>
2024-10-27 09:50:05 +0530

    updates

--- app.py
+++ app.py
@@ -2,29 +2,31 @@ import google.generativeai as genai
 import streamlit as st
 from pytube import YouTube
 import os
-from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptAvailable, VideoUnavailable
+from youtube_transcript_api import YouTubeTranscriptApi
 from dotenv import load_dotenv
 load_dotenv()
 
-
+genai_api_key = st.secrets["GENAI_API_KEY"]
+genai.configure(api_key=genai_api_key)
 
 
 def generate_transcript(url):
-    video_id = url.split('v=')[-1]
-    amp_index = video_id.find('&')
-    if amp_index != -1:
-        video_id = video_id[:amp_index]
-    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'hi'])
-    text = "\n".join([entry['text'] for entry in transcript])
-    return text.strip()
+    # video_id = url.split('v=')[-1]
+    # amp_index = video_id.find('&')
+    # if amp_index != -1:
+    #     video_id = video_id[:amp_index]
+    # transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'hi'])
+    # text = "\n".join([entry['text'] for entry in transcript])
+    # return text.strip()
+    video_id=url.split("=")[1]
+    transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
+    transcript = ""
+    for i in transcript_text:
+        transcript += " " + i["text"]
 
 def generate_output(transcript):
-    genai_api_key = st.secrets["GENAI_API_KEY"]
     prompt = st.secrets["PROMPT"]
-    # genai.configure(api_key=os.getenv("GENAI_API_KEY"))
-    genai.configure(api_key=genai_api_key)
     model = genai.GenerativeModel("gemini-1.5-flash")
-    # prompt=os.getenv("PROMPT")
     response = model.generate_content(prompt+transcript)
     return response.text
 
